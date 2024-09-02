9c55fc9d683281c22271f9f27dec8ffdf76fff90
Diana Barreto <dianibar@gmail.com>
2024-09-02 16:59:31 +1000

    new example

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -1,13 +1,10 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.15.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.11.0, < 6.0.0"
+      version = ">= 3.15"
     }
   }
 }
